62c97c476d5827ead02807a5604331b7b9901d2f
chrohan <chrohan007@gmail.com>
2024-01-01 15:15:17 +0530

    react project first eCommerce

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './DescriptionBox.css'
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="description-nav-box">Description</div>
+            <div className="description-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>An e-commerce webiste is an online platform that facilitates bying and selling of products or seveices oveer
+                the internet serves as a virtual marketplace where businesses and idividual showcase their products, interact with customers,
+                and conduct transactions without the need for a physical presence. Ecommerce webistes hae gained immense popularity due to
+                thir convenient accessibility.
+            </p>
+            <p>
+                E-commerce wesites typically display produts or services a detailed escriptions, images, prices, and nay availables 
+                fslkhsgjsigoghdigorrrrrr
+                gre with relevant information.
+            </p>
+        </div>
+
+    </div>
+  )
+}
